1fffda25023e22078bb06ea7663ffe8a9d8aa1e3
Jerome David <programingjd@gmail.com>
2017-08-18 16:05:44 +0200

    -

--- src/main/kotlin/info/jdavid/server/SecureChannel.kt
+++ src/main/kotlin/info/jdavid/server/SecureChannel.kt
@@ -9,7 +9,6 @@ import java.nio.channels.AsynchronousSocketChannel
 import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLEngine
 import javax.net.ssl.SSLEngineResult
-import javax.net.ssl.SSLHandshakeException
 
 internal class SecureChannel(private val channel: AsynchronousSocketChannel,
                              private val engine: SSLEngine,
@@ -25,6 +24,20 @@ internal class SecureChannel(private val channel: AsynchronousSocketChannel,
   private val application = node.application
   private var exhausted = false
 
+  private val HEX_DIGITS = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                       'a', 'b', 'c', 'd', 'e', 'f')
+
+  private fun hex(byteBuffer: ByteBuffer): String {
+    val n = byteBuffer.remaining()
+    val result = CharArray(3 * n)
+    for (i in 0 until n) {
+      result[3*i] = HEX_DIGITS[byteBuffer[i].toInt().shr(4).and(0x0f)]
+      result[3*i+1] = HEX_DIGITS[byteBuffer[i].toInt().and(0x0f)]
+      result[3*i+2] = if (i%40 == 0) '\n' else ' '
+    }
+    return String(result)
+  }
+
   private suspend fun handshake(channel: AsynchronousSocketChannel,
                                 status: SSLEngineResult.HandshakeStatus,
                                 readDeadline: Long, writeDeadline: Long) {
@@ -38,6 +51,7 @@ internal class SecureChannel(private val channel: AsynchronousSocketChannel,
       SSLEngineResult.HandshakeStatus.NEED_WRAP -> {
         val s = engine.wrap(application, packet).handshakeStatus
         packet.flip()
+        println("Write: " + hex(packet))
         channel.aWrite(packet, writeDeadline - System.nanoTime(), TimeUnit.NANOSECONDS)
 //        channel.write(packet).get()
         handshake(channel, s, readDeadline, writeDeadline)
@@ -46,6 +60,7 @@ internal class SecureChannel(private val channel: AsynchronousSocketChannel,
         channel.aRead(packet, readDeadline - System.nanoTime(), TimeUnit.NANOSECONDS)
 //        channel.read(packet).get()
         packet.flip()
+        println("Read:  " + hex(packet))
         val s = engine.unwrap(packet, application).handshakeStatus
         packet.compact()
         handshake(channel, s, readDeadline, writeDeadline)
