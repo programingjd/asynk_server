6363c8e748340a6fa241525e71d3c67176bf925f
Jerome DAVID <programingjd@gmail.com>
2017-08-25 23:52:30 +0200

    -

--- src/test/kotlin/info/jdavid/server/HttpsTests.kt
+++ src/test/kotlin/info/jdavid/server/HttpsTests.kt
@@ -1,21 +1,26 @@
 package info.jdavid.server
 
 import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
 import javax.net.ssl.HttpsURLConnection
 import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSession
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 
 class HttpsTests: HttpTests() {
   init {
     val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-      override fun getAcceptedIssuers() = null
+      override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
       override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
       override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
     })
-    val sc = SSLContext.getInstance("TLS")
+    val sc = SSLContext.getInstance("SSL")
     sc.init(null, trustAllCerts, java.security.SecureRandom())
     HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+    HttpsURLConnection.setDefaultHostnameVerifier(object: HostnameVerifier {
+      override fun verify(hostname: String?, session: SSLSession?) = true
+    })
   }
 
   override fun scheme() = "https"
