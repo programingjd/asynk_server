0e9cc6c9b3798c14190ef57b397f00c5a711631a
Jerome David <programingjd@gmail.com>
2018-01-14 21:18:08 +0100

    -

--- src/main/kotlin/info/jdavid/server/http/HttpHandler.kt
+++ src/main/kotlin/info/jdavid/server/http/HttpHandler.kt
@@ -41,7 +41,7 @@ abstract class HttpHandler<T: Handler.Acceptance>: AbstractHttpHandler<T>() {
     protected abstract fun bodyByteLength(): Long
     protected abstract fun writeBody(buffer: ByteBuffer)
     private suspend fun error(socket: AsynchronousSocketChannel, buffer: ByteBuffer) {
-      buffer.put(Status.ERROR_RESPONSE.toByteArray(Charsets.US_ASCII))
+      buffer.put(ERROR_RESPONSE)
       socket.aWrite(buffer.flip() as ByteBuffer, 5000L, TimeUnit.MILLISECONDS)
     }
     internal suspend fun write(socket: AsynchronousSocketChannel, buffer: ByteBuffer) {
@@ -54,9 +54,12 @@ abstract class HttpHandler<T: Handler.Acceptance>: AbstractHttpHandler<T>() {
         headers.set(Headers.CONTENT_TYPE, bodyMediaType() ?: MediaType.OCTET_STREAM)
       }
       buffer.put("HTTP/1.1 ${statusCode} ${statusMessage}\r\n".toByteArray(Charsets.ISO_8859_1))
+      for (line in headers.lines) {
+        buffer.put(line.toByteArray(Charsets.ISO_8859_1))
+        buffer.put(CRLF)
+      }
+      buffer.put(CRLF)
       socket.aWrite(buffer.flip() as ByteBuffer, 5000L, TimeUnit.MILLISECONDS)
-
-      // todo write headers
 
       if (body != null) {
         buffer.clear()
@@ -65,6 +68,15 @@ abstract class HttpHandler<T: Handler.Acceptance>: AbstractHttpHandler<T>() {
                       5000L + buffer.capacity() / 1000, TimeUnit.MILLISECONDS)
       }
     }
+  }
+
+  internal companion object {
+    val CRLF = "\r\n".toByteArray(Charsets.US_ASCII)
+    val ERROR_RESPONSE =
+      ("HTTP/1.1 ${Status.INTERNAL_SERVER_ERROR} ${Status.HTTP_STATUSES[Status.INTERNAL_SERVER_ERROR]}\r\n" +
+       "Content-Type: text/plain\r\nContent-Length: 0\r\nConnection: close\r\n\r\n").
+        toByteArray(Charsets.US_ASCII)
+
   }
 
 }
