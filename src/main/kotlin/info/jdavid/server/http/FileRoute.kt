8a7a0e06b08f2c9daf7d2d6833af356e0953b1c3
Jerome David <programingjd@gmail.com>
2018-01-20 01:16:17 +0100

    -

---
+++ src/main/kotlin/info/jdavid/server/http/FileRoute.kt
@@ -0,0 +1,26 @@
+package info.jdavid.server.http
+
+import java.io.File
+import java.net.URLDecoder
+
+open class FileRoute(private val root: File, private val prefix: String = "/"): HttpHandler.Route<File> {
+  init {
+    if (root.isFile) throw IllegalArgumentException()
+    if (prefix.isEmpty() || prefix[0] != '/') throw IllegalArgumentException()
+  }
+  final override fun match(method: Method, uri: String): File? {
+    if (method == Method.GET || method == Method.HEAD && uri.startsWith(prefix)) {
+      val segments = uri.substring(prefix.length).split('/')
+      var file = root
+      for (segment in segments) {
+        if (segment.isEmpty()) {
+          if (file.isFile) return null
+        }
+        else file = File(file, URLDecoder.decode(segment, "UTF-8"))
+      }
+      return file
+    }
+    else return null
+  }
+
+}
