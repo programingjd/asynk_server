6ba88208408cfff947cfba36fec9a15c0acb2403
Jerome DAVID <programingjd@gmail.com>
2018-03-11 13:13:11 +0100

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ repositories {
 
 dependencies {
   compile(kotlin("stdlib-jdk8"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22.3")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-nio:0.22.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22.5")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-nio:0.22.5")
   implementation("org.slf4j:slf4j-api:1.7.25")
   implementation("com.codahale:aes-gcm-siv:0.4.2")
   testImplementation("junit:junit:4.12")
