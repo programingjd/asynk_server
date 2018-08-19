eb1caf16dd4bd42e6a725ab17c383219f2d23653
Jerome David <programingjd@gmail.com>
2018-08-19 14:10:29 +0200

    -

---
+++ src/test/kotlin/info/jdavid/asynk/server/EchoTests.kt
@@ -0,0 +1,82 @@
+package info.jdavid.asynk.server
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.awaitAll
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.channels.toList
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.nio.aConnect
+import kotlinx.coroutines.experimental.nio.aRead
+import kotlinx.coroutines.experimental.nio.aWrite
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.StandardSocketOptions
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousSocketChannel
+import java.util.concurrent.TimeUnit
+
+class EchoTests {
+
+  @Test
+  fun echo() {
+    Server(object: Handler<Nothing?> {
+      override suspend fun context(others: Collection<*>?) = null
+      override suspend fun connect(remoteAddress: InetSocketAddress) = true
+      override suspend fun handle(socket: AsynchronousSocketChannel, buffer: ByteBuffer, context: Nothing?) {
+        while (socket.aRead(buffer, 5, TimeUnit.SECONDS) > -1) {
+          socket.aWrite(buffer.flip(), 5, TimeUnit.SECONDS)
+          buffer.flip()
+        }
+      }
+    }).use {
+      //Thread.sleep(20000L)
+      runBlocking {
+        awaitAll(
+          async {
+            AsynchronousSocketChannel.open().use {
+              it.setOption(StandardSocketOptions.TCP_NODELAY, true)
+              it.setOption(StandardSocketOptions.SO_REUSEADDR, true)
+              it.aConnect(InetSocketAddress(InetAddress.getLoopbackAddress(), 8080))
+              it.aWrite(ByteBuffer.wrap("abc\r\ndef\r\n".toByteArray()))
+              delay(100)
+              it.aWrite(ByteBuffer.wrap("ghi\r\njkl\r\nmno".toByteArray()))
+              it.shutdownOutput()
+              val buffer = ByteBuffer.allocate(128)
+              assertEquals(23, aRead(it, buffer))
+              assertEquals("abc\r\ndef\r\nghi\r\njkl\r\nmno",
+                           String(ByteArray(23).apply { buffer.flip().get(this) }))
+            }
+          },
+          async {
+            AsynchronousSocketChannel.open().use {
+              it.setOption(StandardSocketOptions.TCP_NODELAY, true)
+              it.setOption(StandardSocketOptions.SO_REUSEADDR, true)
+              it.aConnect(InetSocketAddress(InetAddress.getLoopbackAddress(), 8080))
+              it.aWrite(ByteBuffer.wrap("123\r\n".toByteArray()))
+              delay(50)
+              it.aWrite(ByteBuffer.wrap("456\r\n789".toByteArray()))
+              it.shutdownOutput()
+              val buffer = ByteBuffer.allocate(128)
+              assertEquals(13, aRead(it, buffer))
+              assertEquals("123\r\n456\r\n789",
+                           String(ByteArray(13).apply { buffer.flip().get(this) }))
+            }
+          }
+        )
+      }
+    }
+  }
+
+  private suspend fun aRead(socket: AsynchronousSocketChannel, buffer: ByteBuffer) =
+    produce {
+      while (true) {
+        val n = socket.aRead(buffer)
+        if (n > 0) send(n) else break
+      }
+      close()
+    }.toList().sum()
+
+}
--- src/test/kotlin/info/jdavid/asynk/server/Main.kt
+++ src/test/kotlin/info/jdavid/asynk/server/Main.kt
@@ -1,22 +1,15 @@
 package info.jdavid.asynk.server
 
 import info.jdavid.asynk.server.http.*
-import info.jdavid.asynk.server.http.base.AbstractHttpHandler
 import info.jdavid.asynk.server.http.base.DefaultHttpHandler
 import info.jdavid.asynk.server.http.base.SimpleHttpHandler
 import info.jdavid.asynk.server.http.handler.FileHandler
-import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.nio.aWrite
-import kotlinx.coroutines.experimental.runBlocking
 import java.io.File
-import java.io.IOException
-import java.net.HttpURLConnection
 import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.net.URL
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
-import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
   var path = File(Server::class.java.protectionDomain.codeSource.location.path)
@@ -24,9 +17,8 @@ fun main(args: Array<String>) {
     path = path.parentFile
     if (path.isDirectory && File(path, ".git").exists()) break
   }
-//  serveDirectory(path.resolve("src/test/resources"))
+  serveDirectory(path.resolve("src/test/resources"))
 //  connectFor(60000L)
-  connectMany()
 }
 
 fun serveDirectory(directory: File) {
@@ -86,44 +78,3 @@ fun connectFor(millis: Long) {
   }
 }
 
-fun connectMany() {
-  class ExtendedContext(others: Collection<*>?): AbstractHttpHandler.Context(others) {
-    val test: ByteBuffer = (others?.find { it is ExtendedContext } as? ExtendedContext)?.test ?:
-      "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 4\r\nConnection: close\r\n\r\nTest".
-      toByteArray(Charsets.US_ASCII).let {
-        val bytes = ByteBuffer.allocateDirect(it.size); bytes.put(it); bytes
-      }
-  }
-  Server(object : SimpleHttpHandler() {
-    override suspend fun context(others: Collection<*>?) = ExtendedContext(others)
-    override suspend fun connect(remoteAddress: InetSocketAddress) = true
-    override suspend fun handle(acceptance: SimpleHttpHandler.Acceptance, headers: Headers, body: ByteBuffer,
-                                socket: AsynchronousSocketChannel, context: Context) {
-      socket.aWrite((context as ExtendedContext).test.rewind() as ByteBuffer)
-    }
-  }).use {
-    runBlocking {
-      (0..10000).map { i: Int ->
-        async {
-          val t = System.nanoTime()
-          val conn = URL("http://localhost:8080").openConnection() as HttpURLConnection
-          conn.useCaches = false
-          try {
-            val bytes = conn.inputStream.readBytes(512)
-            if (String(bytes) == "Test") println(i)
-          }
-          catch (e: IOException) {
-            e.printStackTrace()
-          }
-          finally {
-            conn.disconnect()
-          }
-          System.nanoTime() - t
-        }
-      }.apply {
-        println()
-        println(TimeUnit.NANOSECONDS.toMicros(map { it.await() }.reduce { acc, l -> acc + l } / size))
-      }
-    }
-  }
-}
